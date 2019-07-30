20b13851123ecf735da0e82edfd83e118474ac10
Caleb Hu <qinxiaoying@gmail.com>
2019-07-30 10:02:00 -0500

    Add solution for problem `B - Water Lily` Link: https://codeforces.com/contest/1199/problem/B Tags: geometry, math, *1000 Ref: https://codeforces.com/contest/1199/submission/58004929

---
+++ codeforces/1199/B.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+public class template {
+	public static void main(String[] args) throws Exception {
+		FastScanner sc = new FastScanner();
+		PrintWriter pw = new PrintWriter(System.out);
+		long n = sc.nextInt();
+		long b = sc.nextInt();
+		double z = b*b-n*n;
+		double y = 2*n;
+		double a = z/y;
+		pw.printf("%.13f\n", a);
+		pw.close();
+	}
+}
+@SuppressWarnings("all")
+class FastScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastScanner(String s) {
+        try {
+            br = new BufferedReader(new FileReader(s));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public FastScanner() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String nextToken() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    long nextLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(nextToken());
+    }
+}
