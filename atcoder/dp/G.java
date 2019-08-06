b8d95f4e798a81fa07919cda356ae7c07dca877c
Caleb Hu <qinxiaoying@gmail.com>
2019-08-06 11:57:00 -0500

    Add solution for problem `G - Longest Path` Link: https://atcoder.jp/contests/dp/tasks/dp_g Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/6747391

--- atcoder/dp/G.java
+++ atcoder/dp/G.java
@@ -1,156 +1,91 @@
-
-// Problem : G - Longest Path
-// Contest : Educational DP Contest
-// URL : https://atcoder.jp/contests/dp/tasks/dp_g
-// Memory Limit : 1024 MB
-// Time Limit : 2000 ms
-// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
-
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
+import java.math.BigInteger;
 public class Main {
-	
 	public static void main(String[] args) throws Exception {
-		FastReader scan = new FastReader();
-		PrintWriter out = new PrintWriter(System.out);
-		//PrintWriter out = new PrintWriter("file.out");
-		Task solver = new Task();
-		//int t = scan.nextInt();
-		int t = 1;
-		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
-		out.close();
+		FastScanner sc = new FastScanner();
+		PrintWriter pw = new PrintWriter(System.out);
+		int l = sc.nextInt();
+		int t = sc.nextInt();
+		Node[] n = new Node[l];
+		for(int i=0;i<l;i++)n[i]=new Node(i+1);
+		int out =0;
+		for(int i=0;i<t;i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			n[x-1].addc(n[y-1]);
+		}
+		for(int i=0;i<l;i++) {
+			out=Math.max(out, getLayer(n[i]));
+		}
+		pw.println(out);
+		pw.close();
 	}
-
-	static class Task {
-		static final int inf = Integer.MAX_VALUE;
-		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
-			int n = sc.nextInt();
-			HashSet<Integer>[] adjl1 = new HashSet[n];
-			HashSet<Integer>[] adjl2 = new HashSet[n];
-			int m = sc.nextInt();
-			for(int i=0;i<n;i++){
-				adjl1[i]=new HashSet<Integer>();
-				adjl2[i] = new HashSet<Integer>();
+	public static int getLayer(Node n) {
+		if(n.layer>-1)return n.layer;
+		else if(n.c.isEmpty())return 0;
+		else {
+			int temp =0;
+			for(Node x : n.c) {	
+				temp=Math.max(temp, getLayer(x)+1);
 			}
-			for(int i = 0;i < m;i++){
-				int a = sc.nextInt()-1;
-				int b = sc.nextInt()-1;
-				adjl1[a].add(b);
-				adjl2[b].add(a);
-			}
-			Queue<Integer> q = new LinkedList<Integer>();
-			for(int i = 0;i < n; i++){
-				if(adjl1[i].size() == 0){
-					q.add(i);
-				}
-			}
-			int bfs[] = new int[n];
-			while(!q.isEmpty()){
-				int t = q.poll();
-				for(int x : adjl2[t]){
-					if(bfs[x]<bfs[t]+1){
-						bfs[x]=bfs[t]+1;
-					}
-					adjl1[x].remove(t);
-					if(adjl1[x].size() == 0){
-						q.add(x);
-					}
-				}
-			}
-			int max = 0;
-			for(int x : bfs){
-				max=Math.max(x,max);
-			}
-			pw.println(max);
+			n.layer=temp;
+			return temp;
 		}
 	}
-	long binpow(long a, long b, long m) {
-		a %= m;
-		long res = 1;
-		while (b > 0) {
-			if ((b & 1) == 1)
-				res = res * a % m;
-			a = a * a % m;
-			b >>= 1;
+	static class Node{
+		int layer;
+		int val;
+		ArrayList<Node> c;
+		public Node(int v) {
+			val=v;
+			layer=-1;
+			c=new ArrayList<Node>();
 		}
-		return res;
-	}
-	static class tup implements Comparable<tup>{
-		int a, b;
-		tup(int a,int b){
-			this.a=a;
-			this.b=b;
-		}
-		@Override
-		public int compareTo(tup o){
-			return Integer.compare(o.b,b);
+		public void addc(Node n) {
+			c.add(n);
 		}
 	}
-	static void shuffle(int[] a) {
-		Random get = new Random();
-		for (int i = 0; i < a.length; i++) {
-			int r = get.nextInt(a.length);
-			int temp = a[i];
-			a[i] = a[r];
-			a[r] = temp;
-		}
-	}
-
-	static void shuffle(long[] a) {
-		Random get = new Random();
-		for (int i = 0; i < a.length; i++) {
-			int r = get.nextInt(a.length);
-			long temp = a[i];
-			a[i] = a[r];
-			a[r] = temp;
-		}
-	}
-
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
-
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
-
-		public FastReader(String s) throws FileNotFoundException {
-			br = new BufferedReader(new FileReader(new File(s)));
-		}
-
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
-
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-
-		long nextLong() {
-			return Long.parseLong(next());
-		}
-
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
-
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-	}
-
+}
+@SuppressWarnings("all")
+class FastScanner {
+    BufferedReader br;
+    StringTokenizer st;
+ 
+    public FastScanner(String s) {
+        try {
+            br = new BufferedReader(new FileReader(s));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+ 
+    public FastScanner() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+ 
+    String nextToken() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+ 
+    int nextInt() {
+        return Integer.parseInt(nextToken());
+    }
+ 
+    long nextLong() {
+        return Long.parseLong(nextToken());
+    }
+ 
+    double nextDouble() {
+        return Double.parseDouble(nextToken());
+    }
 }
