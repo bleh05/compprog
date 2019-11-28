b633cabd34a6cd076f41aed1d9ad9de3ca1323f7
Caleb Hu <qinxiaoying@gmail.com>
2019-11-28 15:39:00 -0600

    Add solution for problem `C - Sweets Eating` Link: https://codeforces.com/contest/1253/problem/C Tags: dp, greedy, math, sortings, *1500 Ref: https://codeforces.com/contest/1253/submission/65931939

---
+++ codeforces/1253/C.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.util.*;
+
+public class template {
+	public static void main(String[] args) throws IOException {
+		FastReader scan = new FastReader();
+		//PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("countcross.out")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		Task solver = new Task();
+		//int t = scan.nextInt();
+		int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+	static class Task {
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			int n =sc.nextInt();
+			int k  =sc.nextInt();
+			int[] x = new int[n];
+			for(int i=0;i<n;i++){
+				x[i]=sc.nextInt();
+			}
+			long sums[] = new long[k];
+			Arrays.sort(x);
+			long[] ans = new long[n];
+			pw.print(x[0]);
+			sums[0]+=x[0];
+			ans[0]=x[0];
+			for(int i=1;i<n;i++){
+				sums[i%k]+=x[i];
+				ans[i]=ans[i-1]+sums[i%k];
+				pw.print(" "+ans[i]);
+			}
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
