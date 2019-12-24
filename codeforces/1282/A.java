a695239642258b22c1c9ea5df8b13b2f00b8a638
Caleb Hu <qinxiaoying@gmail.com>
2019-12-24 08:41:00 -0600

    Add solution for problem `A - Temporarily unavailable` Link: https://codeforces.com/contest/1282/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1282/submission/67528061

---
+++ codeforces/1282/A.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class template {
+	public static void main(String[] args) throws IOException {
+		FastReader scan = new FastReader();
+		//PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("cowmbat.out")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		Task solver = new Task();
+		int t = scan.nextInt();
+		//int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+	static class Task {
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+		    int s= sc.nextInt();
+		    int m = sc.nextInt();
+		    int n = Math.min(s,m);
+		    int end = Math.max(s,m);
+		    int pl = sc.nextInt();
+		    int rad = sc.nextInt();
+		    int minbd = Math.min(end,Math.max(n,pl-rad));
+		    int maxbd = Math.max(n,Math.min(end,rad+pl));
+		    pw.println(end-n-(maxbd-minbd));
+		}
+		/*
+		public long exp(long base, int power){
+			if(power == 0) return 1;
+			if(power == 1) return (base + mod) % mod;
+			long ans = exp(base,power/2);
+			ans = (ans*ans + mod) % mod;
+			if(power%2 == 1) ans = (ans*base + mod) % mod;
+			return (ans + mod) % mod;
+		}*/
+
+	}
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
