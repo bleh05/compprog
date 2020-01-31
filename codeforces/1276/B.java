37848243e31e4823c677db726328c0489fab2e77
Caleb Hu <qinxiaoying@gmail.com>
2020-01-30 19:28:00 -0600

    Add solution for problem `B - Two Fairs` Link: https://codeforces.com/contest/1276/problem/B Tags: combinatorics, dfs and similar, dsu, graphs, *1900 Ref: https://codeforces.com/contest/1276/submission/69872546

---
+++ codeforces/1276/B.java
@@ -0,0 +1,147 @@
+    import java.io.*;
+    import java.util.*;
+
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            FastReader scan = new FastReader();
+            //PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("marathon.out")));
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+            Task solver = new Task();
+            int t = scan.nextInt();
+
+            //int t = 1;
+            for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+            out.close();
+        }
+        static class Task {
+            public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+                int n = sc.nextInt();
+                int m = sc.nextInt();
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                ArrayList<ArrayList<Integer>> adjl = new ArrayList<>();
+                for(int i=0;i<=n;i++){
+                    adjl.add(new ArrayList<>());
+                }
+                for(int i=0;i<m;i++){
+                    int x =sc .nextInt();
+                    int y = sc.nextInt();
+                    adjl.get(x).add(y);
+                    adjl.get(y).add(x);
+                }
+                Queue<Integer> q = new LinkedList<>();
+                boolean visited1 []= new boolean[n+1];
+                boolean visited2 []= new boolean[n+1];
+                q.add(a);
+                visited1[b]=true;
+                visited2[a]=true;
+                while(!q.isEmpty()){
+                    int x= q.poll();
+                    visited1[x]=true;
+                    for(int s : adjl.get(x)){
+                        if(!visited1[s]){
+                            q.add(s);
+                        }
+                    }
+                }
+                q.add(b);
+                while(!q.isEmpty()){
+                    int x= q.poll();
+                    visited2[x]=true;
+                    for(int s : adjl.get(x)){
+                        if(!visited2[s]){
+                            q.add(s);
+                        }
+                    }
+                }
+                long z = 0;
+                long y = 0;
+                for(int i=1;i<=n;i++){
+                    if(visited1[i]&&!visited2[i]){
+                        z++;
+                    }
+                    if(!visited1[i]&&visited2[i]){
+                        y++;
+                    }
+                }
+                pw.println(z*y);
+            }
+        }
+        static class tup implements Comparable<tup>{
+            int a, b;
+            tup(int a, int b){
+                this.a=a;
+                this.b=b;
+            }
+
+            @Override
+            public int compareTo(tup o) {
+                return a!=o.a?Long.compare(a,o.a):Long.compare(o.b,b);
+            }
+        }
+        static void shuffle(long[] a) {
+            Random get = new Random();
+            for (int i = 0; i < a.length; i++) {
+                int r = get.nextInt(a.length);
+                long temp = a[i];
+                a[i] = a[r];
+                a[r] = temp;
+            }
+        }
+        static void shuffle(int[] a) {
+            Random get = new Random();
+            for (int i = 0; i < a.length; i++) {
+                int r = get.nextInt(a.length);
+                int temp = a[i];
+                a[i] = a[r];
+                a[r] = temp;
+            }
+        }
+
+        static class FastReader {
+            BufferedReader br;
+            StringTokenizer st;
+
+            public FastReader() {
+                br = new BufferedReader(new InputStreamReader(System.in));
+            }
+
+            public FastReader(String s) throws FileNotFoundException {
+                br = new BufferedReader(new FileReader(new File(s)));
+            }
+
+            String next() {
+                while (st == null || !st.hasMoreElements()) {
+                    try {
+                        st = new StringTokenizer(br.readLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return st.nextToken();
+            }
+
+            int nextInt() {
+                return Integer.parseInt(next());
+            }
+
+            long nextLong() {
+                return Long.parseLong(next());
+            }
+
+            double nextDouble() {
+                return Double.parseDouble(next());
+            }
+
+            String nextLine() {
+                String str = "";
+                try {
+                    str = br.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return str;
+            }
+        }
+
+    }
