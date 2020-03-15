0506b42f45f4bebbfabd7071cd696d87c01c9036
Caleb Hu <qinxiaoying@gmail.com>
2020-03-14 21:21:00 -0500

    Add solution for problem `A - Kth Term` Link: https://atcoder.jp/contests/panasonic2020/tasks/panasonic2020_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/panasonic2020/submissions/10905332

---
+++ atcoder/panasonic2020/A.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FastReader scan = new FastReader();
+        //PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("taming.out")));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        Task solver = new Task();
+        //int t = scan.nextInt();
+        int t = 1;
+        for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+        out.close();
+    }
+    static class Task {
+        public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+        	int[] x = new int[]{1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+        	pw.println(x[sc.nextInt()-1]);
+        }
+
+    }
+    static class tup implements Comparable<tup>{
+        int a, b,c;
+        tup(){};
+        tup(int a, int b, int c){
+            this.a=a;
+            this.b=b;
+            this.c=c;
+        }
+
+        @Override
+        public int compareTo( tup o2) {
+            return o2.b!=b?Integer.compare(o2.b,b):Integer.compare(o2.a,a);
+        }
+    }
+    static void shuffle(long[] a) {
+        Random get = new Random();
+        for (int i = 0; i < a.length; i++) {
+            int r = get.nextInt(a.length);
+            long temp = a[i];
+            a[i] = a[r];
+            a[r] = temp;
+        }
+    }
+    static void shuffle(int[] a) {
+        Random get = new Random();
+        for (int i = 0; i < a.length; i++) {
+            int r = get.nextInt(a.length);
+            int temp = a[i];
+            a[i] = a[r];
+            a[r] = temp;
+        }
+    }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
