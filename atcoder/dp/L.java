a9738e59d549d7cf167b76e813129cc6848e9919
Caleb Hu <qinxiaoying@gmail.com>
2020-04-08 22:50:00 -0500

    Add solution for problem `L - Deque` Link: https://atcoder.jp/contests/dp/tasks/dp_l Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/11684721

---
+++ atcoder/dp/L.java
@@ -0,0 +1,137 @@
+
+// Problem : L - Deque
+// Contest : Educational DP Contest
+// URL : https://atcoder.jp/contests/dp/tasks/dp_l
+// Memory Limit : 1024 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		FastReader scan = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		//int t = scan.nextInt();
+		int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final long inf = Long.MAX_VALUE;
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			int n = sc.nextInt();
+			long[][] dp = new long[n+4][n+4];
+			long[] arr = new long[n];
+			for(int i=0;i<n;i++){
+				arr[i]=sc.nextInt();
+			}
+			for(int i = n-1; i>= 0;i--){
+				for(int j = i;j < n;j++){
+					if(i==j)
+					{
+						dp[i][j]=arr[i];
+					}
+					else
+					{
+						dp[i][j]=Math.max(arr[i]-dp[i + 1][j], arr[j] - dp[i][j - 1]);
+					}
+				}
+			}
+			pw.println(dp[0][n-1]);
+		}
+	}
+	long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
