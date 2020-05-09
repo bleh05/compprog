08507fed5cc9e5558d18fed2bb67c93e301c6597
Caleb Hu <qinxiaoying@gmail.com>
2020-05-09 11:03:00 -0500

    Add solution for problem `D - Alice, Bob and Candies` Link: https://codeforces.com/contest/1352/problem/D Tags: implementation, *1300 Ref: https://codeforces.com/contest/1352/submission/79545956

---
+++ codeforces/1352/D.java
@@ -0,0 +1,178 @@
+
+// Problem : D. Alice, Bob and Candies
+// Contest : Codeforces - Codeforces Round #640 (Div. 4)
+// URL : https://codeforces.com/contest/1352/problem/D
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+import java.io.*;
+import java.util.*;
+
+public class a implements Runnable{
+	
+    public static void main(String[] args) {
+        new Thread(null, new a(), "process", 1<<26).start();
+    }
+	public void run() {
+		FastReader scan = new FastReader();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		int t = scan.nextInt();
+		//int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final int inf = Integer.MAX_VALUE;
+
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			//CHECK FOR QUICKSORT TLE
+			//***********************//
+			//CHECK FOR INT OVERFLOW
+			//***********************//
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			for(int i = 0; i < n; i++){
+				arr[i] = sc.nextInt();
+			}
+			long a = 0;
+			long b = 0;
+			long c = 0;
+			long d = 0;
+			int turn = 1;
+			int cc = 0;
+			int ptr1 = 0;
+			int ptr2 = n - 1;
+			for(int i = 0; i < n; i++) {
+				if(turn == 1){
+					c += arr[ptr1++];
+					if(ptr2 == ptr1 - 1){
+						a += c;
+						cc++;
+						break;
+					}
+				}
+				if(turn == 1 && c > d){
+					turn = 2;
+					d = 0;
+					a += c;
+					cc++;
+				}
+				if(turn == 2){
+					d += arr[ptr2--];
+					if(ptr2 == ptr1 - 1){
+						b += d;
+						cc++;
+						break;
+					}
+				}
+				if(turn == 2 && d > c){
+					turn = 1;
+					c = 0;
+					b += d;
+					cc++;
+				}
+			}
+			pw.printf("%d %d %d%n", cc, a, b);
+		}
+	}
+	static long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static void sort(int[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static void sort(long[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
