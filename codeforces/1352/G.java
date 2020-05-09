08fc104d3c139e413ee5a0dc8fa35ced6472b65a
Caleb Hu <qinxiaoying@gmail.com>
2020-05-09 09:56:00 -0500

    Add solution for problem `G - Special Permutation` Link: https://codeforces.com/contest/1352/problem/G Tags: constructive algorithms, *1600 Ref: https://codeforces.com/contest/1352/submission/79492971

---
+++ codeforces/1352/G.java
@@ -0,0 +1,164 @@
+
+// Problem : G. Special Permutation
+// Contest : Codeforces - Codeforces Round #640 (Div. 4)
+// URL : https://codeforces.com/contest/1352/problem/G
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+import java.io.*;
+import java.util.*;
+
+public class a implements Runnable{
+	
+    public static void main(String[] args) {
+        new Thread(null, new a(), "process", 1<<26).start();
+    }
+	public void run() {
+		FastReader scan = new FastReader();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		int t = scan.nextInt();
+		//int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final int inf = Integer.MAX_VALUE;
+
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			//CHECK FOR QUICKSORT TLE
+			//***********************//
+			//CHECK FOR INT OVERFLOW
+			//***********************//
+			int n = sc.nextInt();
+			if(n <= 3){
+				pw.println(-1);
+				return;
+			}
+			int[] arr = new int[n];
+			for(int i = 0; i < n - 3; i += 4){
+				arr[i] = i + 2;
+				arr[i + 1] = i + 4;
+				arr[i + 2] = i + 1;
+				arr[i + 3] = i + 3;
+			}
+			if(n % 4 == 3){
+				arr[n - 4] = n - 2;
+				arr[n - 3] = n;
+				arr[n - 2] = n - 4;
+				arr[n - 1] = n - 1;
+			}
+			if(n % 4 == 2){
+				arr[n - 3] = n - 1;
+				arr[n - 2] = n - 3;
+				arr[n - 1] = n;
+			}
+			if(n % 4 == 1){
+				arr[n - 1] = n;
+			}
+			for(int x : arr){
+				pw.print(x + " ");
+			}
+			pw.println();
+		}
+	}
+	static long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static void sort(int[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static void sort(long[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(a.length);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
