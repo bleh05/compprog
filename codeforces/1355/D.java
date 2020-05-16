2b5fc85d5d9ab71a94870966a7a31516fe369ba5
Caleb Hu <qinxiaoying@gmail.com>
2020-05-16 07:41:00 -0500

    Add solution for problem `D - Game With Array` Link: https://codeforces.com/contest/1355/problem/D Tags: constructive algorithms, math, *1400 Ref: https://codeforces.com/contest/1355/submission/80348635

---
+++ codeforces/1355/D.java
@@ -0,0 +1,169 @@
+
+// Problem : D. Game With Array
+// Contest : Codeforces - Codeforces Round #643 (Div. 2)
+// URL : https://codeforces.com/contest/1355/problem/D
+// Memory Limit : 256 MB
+// Time Limit : 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+import java.io.*;
+import java.util.*;
+
+public class a implements Runnable{
+	
+    public static void main(String[] args) {
+        new Thread(null, new a(), "process", 1<<26).start();
+    }
+	public void run() {
+		FastReader scan = new FastReader();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		//int t = scan.nextInt();
+		int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final int inf = Integer.MAX_VALUE;
+
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			//CHECK FOR QUICKSORT TLE
+			//***********************//
+			//CHECK FOR INT OVERFLOW
+			//***********************//
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			if(m / n < 2){
+				pw.println("NO");
+				return;
+			}
+			if(n % 2 == 1 && n * 2 == m){
+				pw.println("YES");
+				for(int i = 0; i < n; i++){
+					pw.print(2 + " ");
+				}
+				pw.println();
+				pw.println(1);
+				return;
+			}
+			pw.println("YES");
+			if(n == 1) {
+				pw.println(m);
+				pw.println(1);
+				return;
+			}
+			m -= n / 2;
+			int iterl = (n + 1) / 2;
+			for(int i = 0; i < n; i++){
+				if(i % 2 == 1){
+					pw.print(1 + " ");
+				}
+				else{
+					pw.print((m + iterl - 1) / iterl + " ");
+					m -= (m + iterl - 1) / iterl;
+					iterl --;
+				}
+			}
+			pw.println();
+			pw.println(2);
+		}
+	}
+	static long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static void sort(int[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static void sort(long[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
