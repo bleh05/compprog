b96d297055eb7445ba2b9713996b2f8a673e60d8
Caleb Hu <qinxiaoying@gmail.com>
2020-06-16 12:01:00 -0500

    Add solution for problem `E - Necklace Assembly` Link: https://codeforces.com/contest/1367/problem/E Tags: brute force, dfs and similar, dp, graphs, greedy, number theory, *1900 Ref: https://codeforces.com/contest/1367/submission/84024638

---
+++ codeforces/1367/E.java
@@ -0,0 +1,170 @@
+
+// Problem : E. Necklace Assembly
+// Contest : Codeforces - Codeforces Round #650 (Div. 3)
+// URL : https://codeforces.com/contest/1367/problem/E
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class a implements Runnable{
+	
+    public static void main(String[] args) {
+        new Thread(null, new a(), "process", 1<<26).start();
+    }
+	public void run() {
+		FastReader scan = new FastReader();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		int t = scan.nextInt();
+		//int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final int inf = Integer.MAX_VALUE;
+
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			//CHECK FOR QUICKSORT TLE
+			//***********************//
+			//CHECK FOR INT OVERFLOW
+			//***********************//
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			int[] freq = new int[26];
+			String str = sc.nextLine();
+			for(int i = 0; i < n; i++) {
+				freq[str.charAt(i) - 'a']++;
+			}
+			HashSet<Integer> pf = new HashSet<Integer>();
+			int ks = k;
+			for(int i = 1; i <= ks; i++) {
+				if(ks%i == 0) {
+					pf.add(i);
+				}
+			}
+			if(ks > 1) {
+				pf.add(ks);
+			}
+			int max = 1;
+			for(int ss = 0; ss < 26; ss++) {
+				max = Math.max(freq[ss], max);
+			}
+			for(int i : pf) {
+				int j;
+				for(j = 1;; j++) {
+					int ctr = 0;
+					for(int ss = 0; ss < 26; ss++) {
+						ctr += freq[ss] / j;
+					}
+					if(ctr < i) {
+						max = Math.max(i*j-i, max);
+						break;
+					}
+				}
+			}
+			pw.println(max);
+		}
+	}
+	static long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static void sort(int[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static void sort(long[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
