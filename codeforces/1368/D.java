b37816c2d1083b45a0877bd9dd1898df89aeb446
Caleb Hu <qinxiaoying@gmail.com>
2020-06-18 10:18:00 -0500

    Add solution for problem `D - AND, OR and square sum` Link: https://codeforces.com/contest/1368/problem/D Tags: bitmasks, greedy, math, *1700 Ref: https://codeforces.com/contest/1368/submission/84199790

---
+++ codeforces/1368/D.java
@@ -0,0 +1,160 @@
+
+// Problem : D. AND, OR and square sum
+// Contest : Codeforces - Codeforces Global Round 8
+// URL : https://codeforces.com/contest/1368/problem/D
+// Memory Limit : 512 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class a implements Runnable{
+	
+    public static void main(String[] args) {
+        new Thread(null, new a(), "process", 1<<26).start();
+    }
+	public void run() {
+		FastReader scan = new FastReader();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		//int t = scan.nextInt();
+		int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final int inf = Integer.MAX_VALUE;
+
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			//CHECK FOR QUICKSORT TLE
+			//***********************//
+			//CHECK FOR INT OVERFLOW
+			//***********************//
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			int[] freq = new int[22];
+			for(int i = 0; i < n; i++) {
+				arr[i] = sc.nextInt();
+				int k = arr[i];
+				for(int j = 0; j < 22; j++) {
+					if((k&1) == 1) {
+						freq[j]++;
+					}
+					k/=2;
+				}
+			}
+			long sum = 0;
+			for(int i = 0; i < n; i++) {
+				int k = 0;
+				for(int j = 21; j >= 0; j--) {
+					if(freq[j] > 0) {
+						k |= (1<<j);
+						freq[j]--;
+					}
+				}
+				sum += 1l*k*k;
+			}
+			pw.println(sum);
+		}
+	}
+	static long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static void sort(int[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static void sort(long[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
