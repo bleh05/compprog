dd9e769553aa505ea2c36a94114dca7350d5ffab
Caleb Hu <qinxiaoying@gmail.com>
2020-06-22 13:25:00 -0500

    Add solution for problem `E - Decypher the String` Link: https://codeforces.com/contest/1117/problem/E Tags: bitmasks, chinese remainder theorem, constructive algorithms, interactive, math, *2200 Ref: https://codeforces.com/contest/1117/submission/84692062

---
+++ codeforces/1117/E.java
@@ -0,0 +1,164 @@
+
+// Problem : E. Decypher the String
+// Contest : Codeforces - Educational Codeforces Round 60 (Rated for Div. 2)
+// URL : https://codeforces.com/contest/1117/problem/E
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class a implements Runnable{
+	
+    public static void main(String[] args) {
+        new Thread(null, new a(), "process", 1<<26).start();
+    }
+	public void run() {
+		FastReader scan = new FastReader();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out), true);
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		//int t = scan.nextInt();
+		int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final int inf = Integer.MAX_VALUE;
+
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			//CHECK FOR QUICKSORT TLE
+			//***********************//
+			//CHECK FOR INT OVERFLOW
+			//***********************//
+			String ln = sc.nextLine();
+			int n = ln.length();
+			StringBuilder a = new StringBuilder();
+			StringBuilder b = new StringBuilder();
+			StringBuilder c = new StringBuilder();
+			for(int i = 0; i < n; i++) {
+				a.append((char)('a' + i/26/26));
+				b.append((char)('a' + i%(26*26)/26));
+				c.append((char)('a' + i%26));
+			}
+			pw.println("? " + a);
+			char[] k = sc.nextLine().toCharArray();
+			pw.println("? " + b);
+			char[] l = sc.nextLine().toCharArray();
+			pw.println("? " + c);
+			char[] m = sc.nextLine().toCharArray();
+			int[] code = new int[n];
+			for(int i = 0; i < n; i++) {
+				int d = 0;
+				d += (k[i]-'a') * 26 * 26;
+				d += (l[i]-'a') * 26;
+				d += (m[i]-'a');
+				code[d] = i;
+			}
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i < n; i++) {
+				sb.append(ln.charAt(code[i]));
+			}
+			pw.println("! " + sb);
+		}
+	}
+	static long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static void sort(int[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static void sort(long[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
