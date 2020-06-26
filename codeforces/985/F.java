2d2eee82d7fdd80046a1cd4dc36ea697f3e6ee32
Caleb Hu <qinxiaoying@gmail.com>
2020-06-26 10:39:00 -0500

    Add solution for problem `F - Isomorphic Strings` Link: https://codeforces.com/contest/985/problem/F Tags: hashing, strings, *2300 Ref: https://codeforces.com/contest/985/submission/85141366

--- codeforces/985/F.java
+++ codeforces/985/F.java
@@ -1,47 +1,255 @@
 
-// Problem : F. Isomorphic Strings
-// Contest : Codeforces - Educational Codeforces Round 44 (Rated for Div. 2)
-// URL : https://codeforces.com/contest/985/problem/F
-// Memory Limit : 256 MB
-// Time Limit : 3000 ms
-// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
 
-public class a implements Runnable{
-	
-    public static void main(String[] args) {
-        new Thread(null, new a(), "process", 1<<26).start();
+public class ECR44F {
+    static final int INF = (int) 1e8;
+    static final int MOD = (int) 1e9 + 7;
+    public static BlockReader input;
+    public static PrintStream output;
+    public static Debug debug;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        init();
+
+        solve();
+
+        destroy();
     }
-	public void run() {
-		FastReader scan = new FastReader();
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
-		//PrintWriter out = new PrintWriter("file.out");
-		Task solver = new Task();
-		//int t = scan.nextInt();
-		int t = 1;
-		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
-		out.close();
-	}
-	static class CharHash {
+
+    public static void init() throws FileNotFoundException {
+        if (System.getProperty("ONLINE_JUDGE") == null) {
+            input = new BlockReader(new FileInputStream("E:\\DATABASE\\TESTCASE\\codeforces\\ECR44F.in"));
+            output = System.out;
+        } else {
+            input = new BlockReader(System.in);
+            output = new PrintStream(new BufferedOutputStream(System.out), false);
+        }
+
+        debug = new Debug();
+        debug.enter("main");
+    }
+
+    public static void solve() {
+        int n = input.nextInteger();
+        int m = input.nextInteger();
+        char[] data = new char[n];
+        input.nextBlock(data, 0);
+
+        CharHash hash31 = new CharHash(data, 31);
+        CharHash hash61 = new CharHash(data, 61);
+
+        boolean[] occupied = new boolean[27];
+        for (int i = 0; i < m; i++) {
+            int x = input.nextInteger() - 1;
+            int y = input.nextInteger() - 1;
+            int l = input.nextInteger();
+
+            Arrays.fill(occupied, false);
+
+            boolean success = true;
+            for (char c = 'a'; c <= 'z'; c++) {
+                int xHash31 = hash31.hash(x, x + l - 1, c);
+                int xHash61 = hash61.hash(x, x + l - 1, c);
+                boolean find = false;
+                for (int mapTo = 0; mapTo < 27; mapTo++) {
+                    if (occupied[mapTo]) {
+                        continue;
+                    }
+                    int yHash31 = hash31.hash(y, y + l - 1, (char) (mapTo + 'a'));
+                    int yHash61 = hash61.hash(y, y + l - 1, (char) (mapTo + 'a'));
+                    if (yHash31 == xHash31 && yHash61 == xHash61) {
+                        occupied[mapTo] = true;
+                        find = true;
+                        break;
+                    }
+                }
+                if (!find) {
+                    success = false;
+                    break;
+                }
+            }
+
+            output.println(success ? "YES" : "NO");
+        }
+    }
+
+    public static void destroy() {
+        output.flush();
+        debug.exit();
+        debug.statistic();
+    }
+
+    public static class Debug {
+        boolean debug = System.getProperty("ONLINE_JUDGE") == null;
+        Deque<ModuleRecorder> stack = new ArrayDeque<>();
+        Map<String, Module> fragmentMap = new HashMap<>();
+
+        public void enter(String module) {
+            if (debug) {
+                stack.push(new ModuleRecorder(getModule(module)));
+            }
+        }
+
+        public Module getModule(String moduleName) {
+            Module module = fragmentMap.get(moduleName);
+            if (module == null) {
+                module = new Module(moduleName);
+                fragmentMap.put(moduleName, module);
+            }
+            return module;
+        }
+
+        public void exit() {
+            if (debug) {
+                ModuleRecorder fragment = stack.pop();
+                fragment.exit();
+            }
+        }
+
+        public void statistic() {
+            if (!debug) {
+                return;
+            }
+
+            if (stack.size() > 0) {
+                throw new RuntimeException("Exist unexited tag");
+            }
+            System.out.println("\n------------------------------------------");
+
+            System.out.println("memory used " + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) >> 20) + "M");
+
+            System.out.println("\n------------------------------------------");
+            for (Module module : fragmentMap.values()) {
+                System.out.println(String.format("Module %s : enter %d : cost %d", module.moduleName, module.enterTime, module.totaltime));
+            }
+
+            System.out.println("------------------------------------------");
+        }
+
+        public static class ModuleRecorder {
+            Module fragment;
+            long time;
+
+            public ModuleRecorder(Module fragment) {
+                this.fragment = fragment;
+                time = System.currentTimeMillis();
+            }
+
+            public void exit() {
+                fragment.totaltime += System.currentTimeMillis() - time;
+                fragment.enterTime++;
+            }
+        }
+
+        public static class Module {
+            String moduleName;
+            long totaltime;
+            long enterTime;
+
+            public Module(String moduleName) {
+                this.moduleName = moduleName;
+            }
+        }
+    }
+
+    public static class BlockReader {
+        static final int EOF = -1;
+        InputStream is;
+        byte[] dBuf;
+        int dPos, dSize, next;
+        StringBuilder builder = new StringBuilder();
+
+        public BlockReader(InputStream is) {
+            this(is, 8192);
+        }
+
+        public BlockReader(InputStream is, int bufSize) {
+            this.is = is;
+            dBuf = new byte[bufSize];
+            next = nextByte();
+        }
+
+        public int nextByte() {
+            while (dPos >= dSize) {
+                if (dSize == -1) {
+                    return EOF;
+                }
+                dPos = 0;
+                try {
+                    dSize = is.read(dBuf);
+                } catch (Exception e) {
+                }
+            }
+            return dBuf[dPos++];
+        }
+
+        public String nextBlock() {
+            builder.setLength(0);
+            skipBlank();
+            while (next != EOF && !Character.isWhitespace(next)) {
+                builder.append((char) next);
+                next = nextByte();
+            }
+            return builder.toString();
+        }
+
+        public void skipBlank() {
+            while (Character.isWhitespace(next)) {
+                next = nextByte();
+            }
+        }
+
+        public int nextInteger() {
+            skipBlank();
+            int ret = 0;
+            boolean rev = false;
+            if (next == '+' || next == '-') {
+                rev = next == '-';
+                next = nextByte();
+            }
+            while (next >= '0' && next <= '9') {
+                ret = (ret << 3) + (ret << 1) + next - '0';
+                next = nextByte();
+            }
+            return rev ? -ret : ret;
+        }
+
+        public int nextBlock(char[] data, int offset) {
+            skipBlank();
+            int index = offset;
+            int bound = data.length;
+            while (next != EOF && index < bound && !Character.isWhitespace(next)) {
+                data[index++] = (char) next;
+                next = nextByte();
+            }
+            return index - offset;
+        }
+
+        public boolean hasMore() {
+            skipBlank();
+            return next != EOF;
+        }
+    }
+
+    public static class CharHash {
         private static int MOD = (int) (1e9 + 7);
         private int[] inverse;
         private int[][] hash;
         private int n;
         private int x;
- 
+
         public CharHash(char[] data, int x) {
             n = data.length;
             inverse = new int[n];
             this.x = x;
             inverse[0] = 1;
-            long inv = binpow(x, MOD - 2, MOD);
+            long inv = pow(x, MOD - 2);
             for (int i = 1; i < n; i++) {
                 this.inverse[i] = (int) (this.inverse[i - 1] * inv % MOD);
             }
- 
+
             hash = new int[27][n];
             for(int i = 0; i < 27; i++) {
                 char c = (char) (i + 'a');
@@ -49,13 +257,26 @@ public class a implements Runnable{
                 long baseInc = 1;
                 for (int j = n - 2; j >= 0; j--) {
                     baseInc = baseInc * x % MOD;
- 
+
                     int dataval = data[j] == c ? 1 : 0;
- 
+
                     hash[i][j] = (int) (((long) hash[i][j + 1] + dataval * baseInc) % MOD);
                 }
             }
         }
+
+        public static long pow(int x, int n) {
+            int bit = 31 - Integer.numberOfLeadingZeros(n);
+            long product = 1;
+            for (; bit >= 0; bit--) {
+                product = product * product % MOD;
+                if (((1 << bit) & n) != 0) {
+                    product = product * x % MOD;
+                }
+            }
+            return product;
+        }
+
         public int hash(int l, int r, char c) {
             int index = c - 'a';
             long hash = this.hash[index][l];
@@ -67,148 +288,6 @@ public class a implements Runnable{
                 hash = hash * inverse[n - 1 - r] % MOD;
             }
             return (int) hash;
-    	}
+        }
     }
-	static class Task {
-		static final int inf = Integer.MAX_VALUE;
-
-		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
-			//CHECK FOR QUICKSORT TLE
-			//***********************//
-			//CHECK FOR INT OVERFLOW
-			//***********************//
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-			String dat = sc.nextLine();
-			CharHash hashed31 = new CharHash(dat.toCharArray(), 31);
-			CharHash hashed61 = new CharHash(dat.toCharArray(), 48491);
-			boolean[] has = new boolean[27];
-			for(int i = 0; i < m; i++) {
-				int x = sc.nextInt() - 1;
-				int y = sc.nextInt() - 1;
-				int l = sc.nextInt();
-				Arrays.fill(has, false);
-				
-				boolean ok = true;
-				l:
-				for(char c = 'a'; c <= 'z'; c++) {
-					int xHash31 = hashed31.hash(x, x + l - 1, c);
-					int xHash61 = hashed61.hash(x, x + l - 1, c);
-					boolean find = false;
-					for(char d = 'a'; d <= 'z' + 1; d++) {
-						if(has[d-'a']) continue;
-						int yHash31 = hashed31.hash(y, y + l - 1, d);
-						int yHash61 = hashed61.hash(y, y + l - 1, d);
-						if(xHash31 == yHash31 && xHash61 == yHash61) {
-							has[d-'a'] = true;
-							find = true;
-							break;
-						}
-					}
-					if(!find) {
-						ok = false;
-						break;
-					}
-				}
-				pw.println(ok ? "YES" : "NO");
-			}
-			
-		}
-	}
-	static long binpow(long a, long b, long m) {
-		a %= m;
-		long res = 1;
-		while (b > 0) {
-			if ((b & 1) == 1)
-				res = res * a % m;
-			a = a * a % m;
-			b >>= 1;
-		}
-		return res;
-	}
-	static void sort(int[] x){
-		shuffle(x);
-		Arrays.sort(x);
-	}
-	static void sort(long[] x){
-		shuffle(x);
-		Arrays.sort(x);
-	}
-	static class tup implements Comparable<tup>{
-		int a, b;
-		tup(int a,int b){
-			this.a=a;
-			this.b=b;
-		}
-		@Override
-		public int compareTo(tup o){
-			return Integer.compare(o.b,b);
-		}
-	}
-	static void shuffle(int[] a) {
-		Random get = new Random();
-		for (int i = 0; i < a.length; i++) {
-			int r = get.nextInt(i + 1);
-			int temp = a[i];
-			a[i] = a[r];
-			a[r] = temp;
-		}
-	}
-
-	static void shuffle(long[] a) {
-		Random get = new Random();
-		for (int i = 0; i < a.length; i++) {
-			int r = get.nextInt(i + 1);
-			long temp = a[i];
-			a[i] = a[r];
-			a[r] = temp;
-		}
-	}
-
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
-
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
-
-		public FastReader(String s) throws FileNotFoundException {
-			br = new BufferedReader(new FileReader(new File(s)));
-		}
-
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
-
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-
-		long nextLong() {
-			return Long.parseLong(next());
-		}
-
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
-
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-	}
-
 }
