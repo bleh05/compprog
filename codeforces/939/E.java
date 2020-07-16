1d88815fda29cf0f5d35ad43175ff6274b395fcb
Caleb Hu <qinxiaoying@gmail.com>
2020-07-16 17:56:00 -0500

    Add solution for problem `E - Maximize!` Link: https://codeforces.com/contest/939/problem/E Tags: binary search, greedy, ternary search, two pointers, *1800 Ref: https://codeforces.com/contest/939/submission/87028806

---
+++ codeforces/939/E.java
@@ -0,0 +1,162 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class a implements Runnable{
+	
+    public static void main(String[] args) {
+        new Thread(null, new a(), "process", 1<<26).start();
+    }
+	public void run() {
+		FastReader scan = new FastReader();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		//int t = scan.nextInt();
+		int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final int inf = Integer.MAX_VALUE;
+		ArrayList<Long> pref = new ArrayList<Long>();
+		int max = 0;
+		long sum = 0;
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			//CHECK FOR QUICKSORT TLE
+			//***********************//
+			//CHECK FOR INT OVERFLOW
+			//***********************//
+			int q = sc.nextInt();
+			for(int i = 0; i < q; i++) {
+				int t = sc.nextInt();
+				if(t == 1) {
+					int k= sc.nextInt();
+					pref.add(sum += k);
+					max = k;
+				}
+				if(t == 2) {
+					pw.printf("%.8f%n", tse(0, pref.size() - 1));
+				}
+			}
+		}
+		public double tse(int l, int r) {
+			 while (r - l > 5) {
+		        int m1 = l + (r - l) / 3;
+		        int m2 = r - (r - l) / 3;
+		        double f1 = geta(m1);      //evaluates the function at m1
+		        double f2 = geta(m2);      //evaluates the function at m2
+		        if (f1 < f2)
+		            l = m1;
+		        else
+		            r = m2;
+		    }
+		    double max = 0;
+		    for(; l <= r; l++) {
+		    	max = Math.max(max, geta(l));
+		    }
+		    return max;
+	    } 
+	    double geta(int c) {
+	    	double sums = max + pref.get(c);
+	    	return max - sums / (c + 2);
+	    }
+	}
+	static long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static void sort(int[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static void sort(long[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
