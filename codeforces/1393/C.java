39e4abde44cbdb18c479d98e97b2f0bb917f1048
Caleb Hu <qinxiaoying@gmail.com>
2020-08-07 10:15:00 -0500

    Add solution for problem `C - Pinkie Pie Eats Patty-cakes` Link: https://codeforces.com/contest/1393/problem/C Tags: constructive algorithms, greedy, math, sortings, *1700 Ref: https://codeforces.com/contest/1393/submission/89238655

---
+++ codeforces/1393/C.java
@@ -0,0 +1,162 @@
+
+// Problem : C. Pinkie Pie Eats Patty-cakes
+// Contest : Codeforces - Codeforces Round #662 (Div. 2)
+// URL : https://codeforces.com/contest/1393/problem/C
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class a implements Runnable{
+	
+    public static void main(String[] args) {
+        new Thread(null, new a(), "process", 1<<26).start();
+    }
+	public void run() {
+		FastReader scan = new FastReader();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		int t = scan.nextInt();
+		//int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final int inf = Integer.MAX_VALUE;
+
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			//CHECK FOR QUICKSORT TLE
+			//***********************//
+			//CHECK FOR INT OVERFLOW
+			//***********************//
+			
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			for(int i = 0; i < n; i++) {
+				arr[sc.nextInt() - 1]++;
+			}
+			sort(arr);
+			int max = arr[n - 1];
+			int zz = -1;
+			for(int i = n - 1; i >= 0; i--) {
+				if(arr[i] == max) zz++;
+				else break;
+			}
+			int l = 0; int r = n - zz;
+			while(r - l > 1) {
+				int mid = (l + r) >> 1;
+				if((n - zz) < (mid + 1l) * (max - 1) + 1) {
+					r = mid - 1;
+				}
+				else l = mid;
+			}
+			if((n - zz) >= (r + 1l) * (max - 1) + 1) {
+				pw.println(r);
+			}
+			else {
+				pw.println(l);
+			}
+		}
+	}
+	static long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static void sort(int[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static void sort(long[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
