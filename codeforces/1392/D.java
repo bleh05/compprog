268d359d682ce0ab9cfd3ebd273dd7442f4f66c0
Caleb Hu <qinxiaoying@gmail.com>
2020-08-16 10:31:00 -0500

    Add solution for problem `D - Omkar and Bed Wars` Link: https://codeforces.com/contest/1392/problem/D Tags: dp, greedy, *1700 Ref: https://codeforces.com/contest/1392/submission/90128882

---
+++ codeforces/1392/D.java
@@ -0,0 +1,203 @@
+
+// Problem : D. Omkar and Bed Wars
+// Contest : Codeforces - Codeforces Global Round 10
+// URL : https://codeforces.com/contest/1392/problem/D
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class a implements Runnable{
+	
+    public static void main(String[] args) {
+        new Thread(null, new a(), "process", 1<<26).start();
+    }
+	public void run() {
+		FastReader scan = new FastReader();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		//PrintWriter out = new PrintWriter("file.out");
+		Task solver = new Task();
+		int t = scan.nextInt();
+		//int t = 1;
+		for(int i = 1; i <= t; i++) solver.solve(i, scan, out);
+		out.close();
+	}
+
+	static class Task {
+		static final int inf = Integer.MAX_VALUE;
+
+		public void solve(int testNumber, FastReader sc, PrintWriter pw) {
+			//CHECK FOR QUICKSORT TLE
+			//***********************//
+			//CHECK FOR INT OVERFLOW
+			//***********************//
+			int n = sc.nextInt();
+			String str = sc.nextLine();
+			int[] arr= new int[n];
+			for(int i = 0; i < n; i++) {
+				arr[i] = str.charAt(i) == 'R' ? 1 : 0;
+			}
+			int[] dp = new int[n+1];
+			dp[0] = 0;
+			dp[1] = inf/2;
+			for(int i = 2; i <= n; i++) {
+				dp[i] = dp[i-2] + (arr[i-2]^1) + arr[i-1];
+				if(i > 2) {
+					dp[i] = Math.min(dp[i-3] + (arr[i-3]^1) + arr[i-1], dp[i]);
+				}
+				if(i > 3) {
+					dp[i] = Math.min(dp[i-4] + (arr[i-4]^1) + (arr[i-3]^1) + arr[i-1] + arr[i-2], dp[i]);
+				}
+			}
+			int min = dp[n];
+			str = str.substring(1) + str.charAt(0);
+			for(int i = 0; i < n; i++) {
+				arr[i] = str.charAt(i) == 'R' ? 1 : 0;
+			}
+			dp[0] = 0;
+			dp[1] = inf/2;
+			for(int i = 2; i <= n; i++) {
+				dp[i] = dp[i-2] + (arr[i-2]^1) + arr[i-1];
+				if(i > 2) {
+					dp[i] = Math.min(dp[i-3] + (arr[i-3]^1) + arr[i-1], dp[i]);
+				}
+				if(i > 3) {
+					dp[i] = Math.min(dp[i-4] + (arr[i-4]^1) + (arr[i-3]^1) + arr[i-1] + arr[i-2], dp[i]);
+				}
+			}
+			min = Math.min(dp[n], min);
+			str = str.substring(1) + str.charAt(0);
+			for(int i = 0; i < n; i++) {
+				arr[i] = str.charAt(i) == 'R' ? 1 : 0;
+			}
+			dp[0] = 0;
+			dp[1] = inf/2;
+			for(int i = 2; i <= n; i++) {
+				dp[i] = dp[i-2] + (arr[i-2]^1) + arr[i-1];
+				if(i > 2) {
+					dp[i] = Math.min(dp[i-3] + (arr[i-3]^1) + arr[i-1], dp[i]);
+				}
+				if(i > 3) {
+					dp[i] = Math.min(dp[i-4] + (arr[i-4]^1) + (arr[i-3]^1) + arr[i-1] + arr[i-2], dp[i]);
+				}
+			}
+			min = Math.min(dp[n], min);
+			str = str.substring(1) + str.charAt(0);
+			for(int i = 0; i < n; i++) {
+				arr[i] = str.charAt(i) == 'R' ? 1 : 0;
+			}
+			dp[0] = 0;
+			dp[1] = inf/2;
+			for(int i = 2; i <= n; i++) {
+				dp[i] = dp[i-2] + (arr[i-2]^1) + arr[i-1];
+				if(i > 2) {
+					dp[i] = Math.min(dp[i-3] + (arr[i-3]^1) + arr[i-1], dp[i]);
+				}
+				if(i > 3) {
+					dp[i] = Math.min(dp[i-4] + (arr[i-4]^1) + (arr[i-3]^1) + arr[i-1] + arr[i-2], dp[i]);
+				}
+			}
+			min = Math.min(dp[n], min);
+			pw.println(min);
+		}
+	}
+	static long binpow(long a, long b, long m) {
+		a %= m;
+		long res = 1;
+		while (b > 0) {
+			if ((b & 1) == 1)
+				res = res * a % m;
+			a = a * a % m;
+			b >>= 1;
+		}
+		return res;
+	}
+	static void sort(int[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static void sort(long[] x){
+		shuffle(x);
+		Arrays.sort(x);
+	}
+	static class tup implements Comparable<tup>{
+		int a, b;
+		tup(int a,int b){
+			this.a=a;
+			this.b=b;
+		}
+		@Override
+		public int compareTo(tup o){
+			return Integer.compare(o.b,b);
+		}
+	}
+	static void shuffle(int[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			int temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static void shuffle(long[] a) {
+		Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+			int r = get.nextInt(i + 1);
+			long temp = a[i];
+			a[i] = a[r];
+			a[r] = temp;
+		}
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
