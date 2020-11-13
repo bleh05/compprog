bd43b465fb9f6047e2fae58cc95323bd8dfbebf5
Caleb Hu <qinxiaoying@gmail.com>
2020-11-13 08:36:00 -0600

    Add solution for problem `A - Specific Tastes of Andre ` Link: https://codeforces.com/contest/1438/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1438/submission/98281445

---
+++ codeforces/1438/A.cpp
@@ -0,0 +1,26 @@
+
+// Problem: A. Specific Tastes of Andre
+// Contest: Codeforces - Codeforces Round #682 (Div. 2)
+// URL: https://codeforces.com/contest/1438/problem/0
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+      cout << 1 << " ";
+    }
+    cout << endl;
+  }
+}
