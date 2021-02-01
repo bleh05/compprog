853eda3557e01bba043c7b975ae4c70fca38d1da
Caleb Hu <qinxiaoying@gmail.com>
2021-02-01 16:14:00 -0600

    Add solution for problem `A - Fake NP` Link: https://codeforces.com/contest/805/problem/A Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/805/submission/106234120

---
+++ codeforces/805/A.cpp
@@ -0,0 +1,21 @@
+
+// Problem: A. Fake NP
+// Contest: Codeforces - Codeforces Round #411 (Div. 2)
+// URL: https://codeforces.com/contest/805/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n, m; cin >> n >> m;
+  if(n == m) {
+  	cout << n << endl;
+  }
+	else cout << 2 << endl;
+}
