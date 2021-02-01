960d175e340aceb14ea610be4d6f805bd1717d83
Caleb Hu <qinxiaoying@gmail.com>
2021-02-01 16:23:00 -0600

    Add solution for problem `C - Find Amir` Link: https://codeforces.com/contest/805/problem/C Tags: constructive algorithms, *1000 Ref: https://codeforces.com/contest/805/submission/106234522

---
+++ codeforces/805/C.cpp
@@ -0,0 +1,18 @@
+
+// Problem: C. Find Amir
+// Contest: Codeforces - Codeforces Round #411 (Div. 2)
+// URL: https://codeforces.com/contest/805/problem/C
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+	int n; cin >> n;
+	 cout << (n - 1) / 2;
+}
