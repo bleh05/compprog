ac95312f04f675496e28b3deee3d43419f3619d2
Caleb Hu <qinxiaoying@gmail.com>
2021-07-14 10:17:00 -0500

    Add solution for problem `C - Manhattan Subarrays` Link: https://codeforces.com/contest/1550/problem/C Tags: brute force, geometry, greedy, implementation Ref: https://codeforces.com/contest/1550/submission/122480237

--- codeforces/1550/C.cpp
+++ codeforces/1550/C.cpp
@@ -1,15 +1,16 @@
+
 // Problem: C. Manhattan Subarrays
 // Contest: Codeforces - Educational Codeforces Round 111 (Rated for Div. 2)
 // URL: https://codeforces.com/contest/1550/problem/C
 // Memory Limit: 256 MB
 // Time Limit: 2000 ms
 // Powered by CP Editor (https://github.com/cpeditor/cpeditor)
- 
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int chk(int a, int b, int c) {
-  if (abs(c - b) + abs(b - a) == abs(c - a))
+int chk(int a, int b, int c, int d) {
+  if (abs(c - b) + abs(b - a) + d == abs(c - a))
     return 0;
   else
     return 1;
@@ -28,17 +29,35 @@ int main() {
     }
     ll ans = n + n - 1;
     for (int i = 0; i < n - 2; i++) {
-      if (chk(arr[i], arr[i + 1], arr[i + 2])) {
-        ans++;
+      bool good = true;
+      for (int j = 0; j < 3; j++) {
+        for (int k = 0; k < 3; k++) {
+          if (k == j) continue;
+          for (int l = 0; l < 3; l++) {
+            if (l == k || l == j) continue;
+            if (chk(arr[i + j], arr[i + k], arr[i + l],
+                    abs(abs(l - j) - (abs(l - k) + abs(k - j)))) == 0) {
+              good = false;
+            }
+          }
+        }
       }
+      if (good) ans++;
     }
     for (int i = 0; i < n - 3; i++) {
-      if (chk(arr[i], arr[i + 1], arr[i + 2]) &&
-          chk(arr[i], arr[i + 1], arr[i + 3]) &&
-          chk(arr[i + 1], arr[i + 2], arr[i + 3]) &&
-          chk(arr[i], arr[i + 2], arr[i + 3])) {
-        ans++;
+      bool good = true;
+      for (int j = 0; j < 4; j++) {
+        for (int k = 0; k < 4; k++) {
+          if (k == j) continue;
+          for (int l = 0; l < 4; l++) {
+            if (l == k || l == j) continue;
+            if (chk(arr[i + j], arr[i + k], arr[i + l],
+                    abs(abs(l - j) - (abs(l - k) + abs(k - j)))) == 0)
+              good = false;
+          }
+        }
       }
+      if (good) ans++;
     }
     cout << ans << "\n";
   }
